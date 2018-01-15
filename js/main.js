8d7e76fb1546d6434735b701e3f83e471cce5634
Akhil <Akhi1@users.noreply.github.com>
2018-01-15 19:11:55 +0530

    adding files new template

---
+++ js/main.js
@@ -0,0 +1,94 @@
+
+(function ($) {
+    "use strict";
+
+
+    /*==================================================================
+    [ Focus Contact2 ]*/
+    $('.input100').each(function(){
+        $(this).on('blur', function(){
+            if($(this).val().trim() != "") {
+                $(this).addClass('has-val');
+            }
+            else {
+                $(this).removeClass('has-val');
+            }
+        })    
+    })
+  
+  
+    /*==================================================================
+    [ Validate ]*/
+    var name = $('.validate-input input[name="name"]');
+    var phone = $('.validate-input input[name="phone"]');
+    var date = $('.validate-input input[name="date"]');
+    var time = $('.validate-input input[name="time"]');
+    var price = $('.validate-input input[name="price"]');
+    var advance = $('.validate-input input[name="advance"]');
+    var email = $('.validate-input input[name="email"]');
+    var message = $('.validate-input textarea[name="message"]');
+
+
+    $('.validate-form').on('submit',function(){
+        var check = true;
+
+        if($(name).val().trim() == ''){
+            showValidate(name);
+            check=false;
+        }
+
+        if($(phone).val().trim() == ''){
+            showValidate(phone);
+            check=false;
+        }
+
+        if($(date).val().trim() == ''){
+            showValidate(date);
+            check=false;
+        }
+
+        if($(time).val().trim() == ''){
+            showValidate(time);
+            check=false;
+        }
+
+        if($(price).val().trim() == ''){
+            showValidate(price);
+            check=false;
+        }
+
+        if($(email).val().trim().match(/^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{1,5}|[0-9]{1,3})(\]?)$/) == null) {
+            showValidate(email);
+            check=false;
+        }
+
+        if($(message).val().trim() == ''){
+            showValidate(message);
+            check=false;
+        }
+
+        return check;
+    });
+
+
+    $('.validate-form .input100').each(function(){
+        $(this).focus(function(){
+           hideValidate(this);
+       });
+    });
+
+    function showValidate(input) {
+        var thisAlert = $(input).parent();
+
+        $(thisAlert).addClass('alert-validate');
+    }
+
+    function hideValidate(input) {
+        var thisAlert = $(input).parent();
+
+        $(thisAlert).removeClass('alert-validate');
+    }
+    
+    
+
+})(jQuery);
